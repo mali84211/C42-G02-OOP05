c136dd5a08bd61ed36199e1fbafe9601cc8578c3
mali84211 <mali84211@gmail.com>
2024-07-22 19:28:12 +0300

    Operator Overloading

---
+++ C42-G02-OOP05/Complex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_OOP05
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+
+        public static Complex operator +(Complex Left,Complex Right) 
+        {
+            return new Complex()
+            {
+            Real = Left.Real+Right.Real,
+            Imag = Left.Imag+Right.Imag
+            };
+        }
+        public static Complex operator -(Complex Left, Complex Right) {
+            return new Complex()
+            {
+                Real = (Left?.Real??0) - (Right?.Real??0),
+                Imag = (Left?.Imag??0) + (Right?.Imag??0)
+            };
+        }
+    }
+}
--- C42-G02-OOP05/Program.cs
+++ C42-G02-OOP05/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region V1
+            //Complex C1 = new Complex() { Real = 3, Imag = 5 };
+            //Console.WriteLine(C1);
+
+            //Complex C2 = new Complex() { Real = 2, Imag = 4 };
+            //Console.WriteLine(C2);
+
+            //Complex C3 = C1 + C2;
+            //Console.WriteLine(C3);
+
+            //Complex C3 = C1 - C2;
+            //Console.WriteLine(C3);
+
+            //Complex C3 = default;
+            //   C3 += C1;
+            //Console.WriteLine(C3);
+
+            //C3 -= C1;
+            #endregion
+
+            #region V2
+
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
         }
     }
 }
